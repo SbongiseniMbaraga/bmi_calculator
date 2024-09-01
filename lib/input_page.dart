38a007352e4bb11c1d31d51f9501675a1b55b4c7
SbongiseniMbaraga <Sbongiseni2@hotmail.com>
2024-09-01 12:15:30 +0200

    Done 3

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -81,54 +81,58 @@ class InputPageState extends State<InputPage> {
           ),
           Expanded(
             flex: 2,
-            child: ReusableCard(
-              newColor: kActiveCardColor,
-              cardChild: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'HEIGHT',
-                    style: kLabelTextStyle,
-                  ),
-                  Row(
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                return ReusableCard(
+                  newColor: kActiveCardColor,
+                  cardChild: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.baseline,
-                    textBaseline: TextBaseline.alphabetic,
                     children: [
-                      Text(
-                        height.toString(),
-                        style: kNumberTextStyle,
-                      ),
                       const Text(
-                        'CM',
+                        'HEIGHT',
                         style: kLabelTextStyle,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.baseline,
+                        textBaseline: TextBaseline.alphabetic,
+                        children: [
+                          Text(
+                            height.toString(),
+                            style: kNumberTextStyle,
+                          ),
+                          const Text(
+                            'CM',
+                            style: kLabelTextStyle,
+                          ),
+                        ],
+                      ),
+                      SliderTheme(
+                        data: SliderTheme.of(context).copyWith(
+                          activeTrackColor: Colors.white,
+                          inactiveTrackColor: const Color(0xFF8d8E98),
+                          thumbColor: const Color(0xFFEB1555),
+                          overlayColor: const Color(0x1fEB1555),
+                          thumbShape: const RoundSliderThumbShape(
+                              enabledThumbRadius: 15.0),
+                          overlayShape: const RoundSliderOverlayShape(
+                              overlayRadius: 30.0),
+                        ),
+                        child: Slider(
+                          value: height.toDouble(),
+                          min: 120.0,
+                          max: 220.0,
+                          onChanged: (double newValue) {
+                            setState(() {
+                              height = newValue.round();
+                            });
+                          },
+                        ),
                       ),
                     ],
                   ),
-                  SliderTheme(
-                    data: SliderTheme.of(context).copyWith(
-                      activeTrackColor: Colors.white,
-                      inactiveTrackColor: const Color(0xFF8d8E98),
-                      thumbColor: const Color(0xFFEB1555),
-                      overlayColor: const Color(0x1fEB1555),
-                      thumbShape:
-                          const RoundSliderThumbShape(enabledThumbRadius: 15.0),
-                      overlayShape:
-                          const RoundSliderOverlayShape(overlayRadius: 30.0),
-                    ),
-                    child: Slider(
-                      value: height.toDouble(),
-                      min: 120.0,
-                      max: 220.0,
-                      onChanged: (double newValue) {
-                        setState(() {
-                          height = newValue.round();
-                        });
-                      },
-                    ),
-                  ),
-                ],
-              ),
+                );
+              },
             ),
           ),
           Row(
@@ -255,15 +259,17 @@ class InputPageState extends State<InputPage> {
           ),
           GestureDetector(
             onTap: () {
-              CalculatorBrain calc = CalculatorBrain(height: height, weight: weight);
+              CalculatorBrain calc =
+                  CalculatorBrain(height: height, weight: weight);
 
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => ResultsPage(
-                  bmiResult: calc.calculateBMI(),
-                  resultText: calc.getResult(),
-                  interpretation: calc.getInterpretation(),
-                )),
+                MaterialPageRoute(
+                    builder: (context) => ResultsPage(
+                          bmiResult: calc.calculateBMI(),
+                          resultText: calc.getResult(),
+                          interpretation: calc.getInterpretation(),
+                        )),
               );
             },
             child: Container(
@@ -274,8 +280,8 @@ class InputPageState extends State<InputPage> {
               padding: const EdgeInsets.only(bottom: 20.0),
               child: const Center(
                 child: Text(
-                    'CALCULATE',
-                    style: kLageButtonTextStyle,
+                  'CALCULATE',
+                  style: kLageButtonTextStyle,
                 ),
               ),
             ),
